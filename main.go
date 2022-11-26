2a678c468a7828ea8df07c871ac4124eeb67293a
GetDon <getdon0819@naver.com>
2022-11-26 19:32:40 +0900

    text

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+
+	if f, err := os.Open(".//test.txt"); err != nil {
+		fmt.Println(err)
+	} else {
+		defer f.Close()
+
+		buff := make([]byte, 32)
+		if _, err2 := f.Read(buff); err2 != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(string(buff))
+		}
+	}
 }
